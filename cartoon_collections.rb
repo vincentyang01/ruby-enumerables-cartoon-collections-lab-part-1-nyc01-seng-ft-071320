5f13451dd3bed0637268e2f95b2a930fdb9d0719
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-03 22:44:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,9 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  integers = [1, 2, 3]
-  integers.each_with_index do |num, idx|
-    puts "value #{num} has index #{idx}"
+  
+  array.each do |name|
+    puts "Hello #{name}!"
 end
 
 def list_dwarves(array)
